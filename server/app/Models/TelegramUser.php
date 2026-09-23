f03ab40c503511ea99181957586874adbfc575c7
yishak <yishak.110402@gmail.com>
2026-09-23 19:19:03 +0300

    Add TelegramUser model and TgUserController

--- server/app/Models/TelegramUser.php
+++ server/app/Models/TelegramUser.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['chat_id', 'full_name'])]
 class TelegramUser extends Model
 {
-    //
+    /** @var string */
+    protected $table = 'tg_users';
+    protected $fillable = [
+        'chat_id',
+        'full_name'
+    ];
 }
